644ea63bd7372366fc1b0c155f3a1a6633ac023d
chientrm <chientrm@gmail.com>
2024-01-28 05:30:17 +0000

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  // @ts-nocheck
+  import { PUBLIC_TAG_ID } from "$env/static/public";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      "gtm.start": new Date().getTime(),
+      event: "gtm.js",
+    });
+    const gtmScript = document.createElement("script");
+    gtmScript.src =
+      "https://www.googletagmanager.com/gtm.js?id=" + PUBLIC_TAG_ID;
+    document.head.append(gtmScript);
+  });
+</script>
+
+<a href="/">Home</a>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<h1>SvelteKit GTM Bug</h1>
+Visit<a href="/a">A</a>
+<br />
+Visit <a href="/b">B</a>
